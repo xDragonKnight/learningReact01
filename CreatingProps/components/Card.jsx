b3f729ea350a156a00799f5bfc6d7b49b1790d8c
xDragonKnight <ahmedyar1122@gmail.com>
2026-05-31 01:34:46 +0500

    bug fix

--- CreatingProps/components/Card.jsx
+++ CreatingProps/components/Card.jsx
@@ -7,7 +7,7 @@ function Card(props) {
     <div className='test'>
         <img src={props.imgsrc} alt='Profile Picture'></img>
       <h1>{props.name}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, iusto?</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit Illo, iusto?</p>
 
       <p>Age: {props.age}</p>
       <button>Update</button>
