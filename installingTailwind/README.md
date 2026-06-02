3c8d7449be59dca68acbe671680876ab6115201f
xDragonKnight <ahmedyar1122@gmail.com>
2026-06-02 22:01:45 +0500

    Installing Tailwind Guide

---
+++ installingTailwind/README.md
@@ -0,0 +1,1 @@
+easy peasy lemon squezyy
---
+++ installingTailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="h-screen w-full bg-amber-900">
+      <h4>Hey</h4>
+    </div>
+  )
+}
+
+export default App
