a82971a11c059a9cddb8a7240cd5989f619fcc4d
xDragonKnight <ahmedyar1122@gmail.com>
2026-06-14 14:51:11 +0500

    API Calling AXIOS

---
+++ APICalling/src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react';
+function App() {
+
+const [data, setdata] = useState([])
+
+  async function btnclick(){
+  const response =  await axios.get('https://dogapi.dog/api/v2/facts?limit=3')
+ // console.log(response);
+  
+  setdata(response.data.data)
+
+  console.log(data);
+  
+  
+}
+  return (
+    <div>
+        <h1>Hey There.</h1>
+        <h4>Button bellow will Call API</h4>
+        <button onClick={()=>{
+          btnclick()
+        }}>API</button>
+
+        {
+         data.map(function(elem,idx){
+          return (
+            <h2 key={idx}>Fact {idx+1}, {elem.attributes.body}</h2>
+          )
+         })
+        }
+    </div>
+  )
+}
+
+export default App
